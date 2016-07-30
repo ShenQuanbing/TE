e2b0371e41ddb54a877758a8205ca8362b1ba1b5
dlt-lihongwei <915576784@qq.com>
2016-07-30 16:02:41 +0800

    第一次

--- TE/Form1.Designer.cs
+++ TE/Form1.Designer.cs
@@ -28,10 +28,30 @@ namespace TE
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new Gizmox.WebGUI.Forms.ComboBox();
+            this.button1 = new Gizmox.WebGUI.Forms.Button();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(105, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
             // 
             // Form1
             // 
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
             this.Size = new System.Drawing.Size(284, 354);
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -39,5 +59,8 @@ namespace TE
         }
 
         #endregion
+
+        private Gizmox.WebGUI.Forms.ComboBox comboBox1;
+        private Gizmox.WebGUI.Forms.Button button1;
     }
 }
