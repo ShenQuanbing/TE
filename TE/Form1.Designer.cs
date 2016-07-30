9d1bd2984259bb5bcc7af22e5e9e0c966cc2c145
dlt-lihongwei <915576784@qq.com>
2016-07-30 16:03:47 +0800

    di

--- TE/Form1.Designer.cs
+++ TE/Form1.Designer.cs
@@ -42,11 +42,12 @@ namespace TE
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 156);
+            this.button1.Location = new System.Drawing.Point(105, 194);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- TE/Form1.cs
+++ TE/Form1.cs
@@ -20,5 +20,10 @@ namespace TE
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
