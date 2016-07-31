6a48786c1f47f934e336cb8fca8b7b696295e352
Element <Element@DESKTOP-HVULSTD>
2016-08-01 05:31:59 +0800

    2

--- TE/Form1.Designer.cs
+++ TE/Form1.Designer.cs
@@ -80,7 +80,7 @@ namespace TE
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 62);
+            this.label1.Location = new System.Drawing.Point(53, 70);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 5;
