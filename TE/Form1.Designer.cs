7c351d8ac185dc60a9ed70aac8489b0a874f5a0e
Element <Element@DESKTOP-HVULSTD>
2016-08-01 05:21:09 +0800

    1

--- TE/Form1.Designer.cs
+++ TE/Form1.Designer.cs
@@ -39,7 +39,7 @@ namespace TE
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(81, 200);
+            this.comboBox1.Location = new System.Drawing.Point(81, 213);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 0;
