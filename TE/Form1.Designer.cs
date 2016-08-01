7766f278f38ec0dadd4ba90339396544b002a52b
Shutdown <Shutdown@WANGZONGHAO>
2016-08-01 08:55:40 +0800

    来自PC-B的第二次上传

--- TE/Form1.Designer.cs
+++ TE/Form1.Designer.cs
@@ -90,7 +90,7 @@ namespace TE
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(136, 173);
+            this.label2.Location = new System.Drawing.Point(127, 174);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 6;
